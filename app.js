cbefe387796c852d4264b0188c7930d427e15669
Jane Smith <example@email.com>
2017-02-07 16:56:15 -0800

    Added question 1, cliantro question

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 'use strict';
 
-var wannaPlay = prompt("So you think you know me?", "Yes or No");
-var lowerCase = wannaPlay .toLowerCase
-var upperCase = wannaPlay .toUpperCase
+var wannaPlay = prompt("So you think you know me?", "Yes or No").toLowerCase();
+
 
 if(wannaPlay ==='yes'|| wannaPlay ==='y'){  //ask user to play the game
-        alert("Prove it!");
-    console.log('User answer to question 1: ' + wannaPlay);
+        confirm("Prove it!");
+    console.log('User answer to game invite: ' + wannaPlay);
 }
 else{
-    alert("Play this game to learn some fun Ivy facts!")
-    console.log('User answer to question 1: ' + wannaPlay);
+    confirm("Play this game to learn some fun Ivy facts!")
+    console.log('User answer to game invite: ' + wannaPlay);
 }
 
 //Provide instructions for quiz
-//alert('For each of the following five questions, ask yourself <i>Is is true that Ivy . . .</i>');
+confirm('For each of the following five questions, ask yourself: Is it true that Ivy . . .');  //<i>this text in CSS
 
-//var question1 = prompt("Theorizes that cilantro is the most polarizing of foods?"){
+//start game
+var question1 = prompt("Has a theory that cilantro is the most polarizing of foods?").toLowerCase();
+
+if(question1==='yes' || question1==='y'){
+    alert('Indeed!  Ivy has mounting evidence that all humans are either "cilantro sluts" or "soap tasters".');
+console.log('User answer to question 1: ' + question1);
+}
+else{
+    alert("While candy corn is a close 2nd, Ivy does, indeed, believe that cilantro is 100% divisive.");
+    console.log('User answer to question 1: ' + question1);
+}
 
 
 
