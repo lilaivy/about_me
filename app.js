6c91eb658a24f848696d9a0e826c6bc8040b2111
Jane Smith <example@email.com>
2017-02-07 20:04:13 -0800

    Added Yes or No prompt to user

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ else {
 confirm('For each of the following five questions, ask yourself: Is it true that Ivy . . .');  //<i>this text in CSS
 
 //start game
-var question1 = prompt("Has a theory that cilantro is the most polarizing of foods?").toLowerCase();
+var question1 = prompt("Has a theory that cilantro is the most polarizing of foods?","Yes or No").toLowerCase();
 
 if (question1 === 'yes' || question1 === 'y') {
   alert('Indeed!  Ivy has mounting evidence that all humans are either "cilantro sluts" or "soap tasters".');
@@ -27,7 +27,7 @@ else {
   console.log('User answer to question 1: ' + question1);
 }
 
-var question2 = prompt('Left the country unannounced to trek with sherpas and teach English to children in Nepal.').toLowerCase();
+var question2 = prompt('Left the country unannounced to trek with sherpas and teach English to children in Nepal.',"Yes or No").toLowerCase();
 
 if (question2 === 'yes' || question2 === 'y') {
   alert("I wish! It wasn't Nepal, it was Belieze.  And I wasn't treking with Sherpas, I was farming with Mayans.  The unannounced part was right, though!");
@@ -39,7 +39,7 @@ else {
 }
 
 
-var question3 = prompt('Once complimented Beck, unknowingly, on his "Dope shoestrings!".').toLowerCase();
+var question3 = prompt('Once complimented Beck, unknowingly, on his "Dope shoestrings!".',"Yes or No").toLowerCase();
 if (question3 === 'yes' || question3 === 'y') {
   alert("Tragically, this is a fact.");
   console.log('User answer to question 3: ' + question3);
@@ -49,7 +49,7 @@ else {
   console.log('User answer to question 3: ' + question3);
 }
 
-var question4 = prompt('Ended a cross country road trip in Portland and never left.').toLowerCase();
+var question4 = prompt('Ended a cross country road trip in Portland and never left.',"Yes or No").toLowerCase();
 if (question4 === 'yes' || question4 === 'y') {
   alert("It's mostly true.  I had to return to NYC to tie up some loose ends first.");
   console.log('User answer to question 4: ' + question4);
@@ -59,7 +59,7 @@ else {
   console.log('User answer to question 4: ' + question4);
 }
 
-var question5 = prompt("Once won $700 in a fierce game of BINGO.").toLowerCase();
+var question5 = prompt("Once won $700 in a fierce game of BINGO.","Yes or No").toLowerCase();
 if (question5 === 'yes' || question5 === 'y') {
   alert("Damn straight!");
   console.log('User answer to question 5: ' + question5);
