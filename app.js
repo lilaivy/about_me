b6269f808320615d65215bd3fa523cc234f5ab69
Jane Smith <example@email.com>
2017-02-07 16:14:17 -0800

    Asked user to play game and provided instructions

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var wannaPlay = prompt("So you think you know me?", "Yes or No");
+var lowerCase = wannaPlay .toLowerCase
+var upperCase = wannaPlay .toUpperCase
+
+if(wannaPlay ==='yes'|| wannaPlay ==='y'){  //ask user to play the game
+        alert("Prove it!");
+    console.log('User answer to question 1: ' + wannaPlay);
+}
+else{
+    alert("Play this game to learn some fun Ivy facts!")
+    console.log('User answer to question 1: ' + wannaPlay);
+}
+
+//Provide instructions for quiz
+//alert('For each of the following five questions, ask yourself <i>Is is true that Ivy . . .</i>');
+
+//var question1 = prompt("Theorizes that cilantro is the most polarizing of foods?"){
+
+
+
